8773715b3652e4e39b23f2f98a8da491b0af0246
Ana Julia Bittencourt <anajuliabit@gmail.com>
2020-07-25 15:18:07 -0300

    feat: getting credentials access client

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,39 +1,41 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+const https = require('https');
+const fs = require('fs');
+const qs = require('querystring');
 
 @Injectable()
 export class AuthService {
   async getCredentialsAcessClient() {
-    console.log(require('path').join(__dirname));
-    console.log(
-      require('path').resolve(__dirname, '../config/client_certificate.crt'),
-    );
-    const httpsAgent = new (require('https').Agent({
-      cert: require('fs').readFileSync(
-        require('path').resolve(__dirname, '../config/client_certificate.crt'),
-      ),
-      key: require('fs').readFileSync(
-        require('path').resolve(__dirname, '../config/client_private_key.key'),
-      ),
+    const httpsAgent = new https.Agent({
+      cert: fs.readFileSync(process.cwd() + '/config/client_certificate.crt'),
+      key: fs.readFileSync(process.cwd() + '/config/client_private_key.key'),
       rejectUnauthorized: false,
-    }))();
+    });
+
+    const body = {
+      grant_type: 'client_credentials',
+      scope: 'accounts openid',
+    };
 
     const instance = axios.create({
       httpsAgent,
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
         Accept: 'application/json',
-        Authorization: 'Basic ASDF',
+        Authorization:
+          'Basic MzM0ZGExNjUtNzc5OS00ZTZiLTgxYmEtNmU0NjMxYzJmM2Y5OjIyOTM4NDRlLTcxZDgtNDBjYS1iZGRhLTEyYjA4YzUzMGJhNw==',
       },
     });
     const response = await instance.post(
       'https://as1.tecban-sandbox.o3bank.co.uk/token',
-      {
-        grand_type: 'client_credentials',
-        scope: 'accounts openid',
-      },
+      qs.stringify(body),
     );
-    console.log(response.data);
+
+    if (response.status !== 200) {
+      // @TODO Handle error
+      console.log(response);
+    }
     return response.data;
   }
 }
